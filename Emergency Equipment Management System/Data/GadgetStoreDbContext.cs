67a30b765cc5641beebcc04d1bebd5977d2f4c17
EhabsaedAfaneh <eahabs22@gmail.com>
2026-06-06 13:21:32 +0300

    Update GadgetStoreDbContext.cs
    fgdtyh

--- Emergency Equipment Management System/Data/GadgetStoreDbContext.cs
+++ Emergency Equipment Management System/Data/GadgetStoreDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using GadgetStore.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 namespace GadgetStore.Data
 {
     public class GadgetStoreDbContext : IdentityDbContext<IdentityUser>
