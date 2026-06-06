414dc550a8df973567097236b6df72e35f13090f
EhabsaedAfaneh <eahabs22@gmail.com>
2026-06-06 13:29:10 +0300

    Update GadgetStoreDbContext.cs
    changing

--- Emergency Equipment Management System/Data/GadgetStoreDbContext.cs
+++ Emergency Equipment Management System/Data/GadgetStoreDbContext.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using GadgetStore.Models;
 using Microsoft.EntityFrameworkCore;
-//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//change page name;
 namespace GadgetStore.Data
 {
     public class GadgetStoreDbContext : IdentityDbContext<IdentityUser>
